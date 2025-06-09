0e3cc8e1e49ce1c32e995d35801baeddccb31a3d
Saifan Aremenak <saifanaremenak@gmail.com>
2025-06-09 13:21:51 +0200

    modified

--- snowmann.py
+++ snowmann.py
@@ -39,15 +39,33 @@ def get_random_word():
 
 def play_game():
     secret_word = get_random_word()
+
     print("Welcome to Snowman Meltdown!")
     print("Secret word selected: " + secret_word)  # for testing, later remove this line
-
+    mistakes = 0
     # TODO: Build your game loop here.
     # For now, simply prompt the user once:
+    while mistakes< len(STAGES):
+          guess = input("Guess a letter: ").lower()
+          display_game_state(mistakes, secret_word, guess)
+          if not is_letter_in_guess(guess, secret_word):
+              mistakes += 1
 
-    guess = input("Guess a letter: ").lower()
-    print("You guessed:", guess)
+          print("You guessed:", guess)
 
 
+def  display_game_state(mistakes, secret_word, guessed_letters):
+     print(STAGES[mistakes])
+     display_word = ""
+     for letter in secret_word:
+         if letter == guessed_letters:
+            #if is_letter_in_guess(guessed_letters,secret_word):
+             display_word+= letter
+         else:
+             display_word+= "_"
+     print(display_word)
+def is_letter_in_guess(guess_letter,secret_word):
+
+    return guess_letter in secret_word
 if __name__ == "__main__":
     play_game()
